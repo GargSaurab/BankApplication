fc6dcf53579166ba6d3cdf990fc940a9ca4f4fc5
GargSaurab <saurab5719@gmai.com>
2024-07-26 10:19:21 +0530

    Optimize some things with CommonResponse and GlobalExceptionHandler

--- src/main/java/com/app/Dto/CommonResponse.java
+++ src/main/java/com/app/Dto/CommonResponse.java
@@ -1,8 +1,5 @@
 package com.app.Dto;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class CommonResponse extends CustomInfoResponse{
 
     public Object data;
--- src/main/java/com/app/ExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/app/ExceptionHandler/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.app.ExceptionHandler;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,33 +13,39 @@ import com.app.Dto.StatusCode;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @Autowired
-    private CommonResponse response;
-   
     //ResourceNotFoundException
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> handlerResourceNotFoundException(ResourceNotFoundException e) {
+        
+        CommonResponse response = new CommonResponse();
+        
         System.out.println("Resource not found in" + e);
-
+        
         response.info.code = StatusCode.server_Error;
         response.info.message = "Unexcpected Error! while searching the data.";
         return ResponseEntity.ok(response);
     }
-
+    
     //InvalidInputException
     @ExceptionHandler(InvalidInputException.class)
     public ResponseEntity<?> handlerInvalidInputException(InvalidInputException e) {
+       
+        CommonResponse response = new CommonResponse();
+        
+        
         System.out.println("Invalid input in" + e.getMessage());
-
+        
         response.info.code = StatusCode.bad_request;
         response.info.message = String.format("Invalid input please check again! %s", e.getMessage()) ;
         return ResponseEntity.ok(response);
     }
-
+    
     //BadCredentialsException
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<?> HandlerBadCredentialsException(BadCredentialsException e) {
-
+        
+        CommonResponse response = new CommonResponse();
+        
         response.info.code = StatusCode.bad_request;
         response.info.message = "Wrong Credentials";
         return ResponseEntity.ok(response);
@@ -49,12 +54,14 @@ public class GlobalExceptionHandler {
     //Exception
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handlerException(Exception e) {
-
+        
+        CommonResponse response = new CommonResponse();
+        
         e.printStackTrace();
-    
+        
         response.info.code = StatusCode.server_Error;
         response.info.message = "Some unexpected error occured! please try again.";
-       return ResponseEntity.ok(response);
+        return ResponseEntity.ok(response);
     }
-
+    
 }
